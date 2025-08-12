fcb515be0bac2a76599f2656c0d5e735dc602fe7
Tomy Doda <tomy.doda@gmail.com>
2025-08-12 14:10:39 +0200

    Create functions file, with function to read parameters files

---
+++ scripts/functions.py
@@ -0,0 +1,65 @@
+"""
+Functions used for the two-box lake model.
+
+Author: T. Doda, Faculty of Geoscience and Environment, Institute of Earth Surface Dynamics, University of Lausanne
+Contact: tomy.doda@gmail.com
+Date: xx.dd.2025
+"""
+
+import os
+import math
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, timezone, UTC
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def load_parameters(file):
+    """Function load_parameters
+
+    Load model parameters from a csv file.
+
+    Inputs:
+        file (string): file name (including path).
+        
+    Outputs:
+        values_dict (dictionary): values of the parameters from the csv file
+        units_dict (dictionary): units of the parameters from the csv file
+        ref_dict (dictionary): reference of the parameter values from the csv file
+    """
+    
+    df=pd.read_csv(file, sep=",",index_col=0,header=0,dtype=str) 
+    data_dict=df.to_dict()
+    if "Values" in data_dict.keys():
+        values_dict=data_dict["Values"]
+        for key in values_dict.keys(): # Convert values to float if possible
+            if "Units" in data_dict.keys():
+                if data_dict["Units"][key]=="yyyymmdd":
+                    values_dict[key]=datetime.strptime(values_dict[key],"%Y%m%d") 
+                elif data_dict["Units"][key]=="yyyymmddHHMM":
+                    values_dict[key]=datetime.strptime(values_dict[key],"%Y%m%d%H%M")
+                elif data_dict["Units"][key]=="HHMM":
+                    values_dict[key]=datetime.strptime(values_dict[key],"%H%M")
+            try:
+                values_dict[key]=float(values_dict[key])
+            except:
+                continue
+    else:
+        raise Exception("Parameter values are missing")
+        
+    if "Units" in data_dict.keys():
+        units_dict=data_dict["Units"]
+    else:
+        print("*WARNING*: Parameter units are missing")
+        units_dict=dict()
+        
+    if "Reference" in data_dict.keys():
+        ref_dict=data_dict["Reference"]
+    else:
+        ref_dict=dict()
+    
+    
+    return values_dict, units_dict, ref_dict
+
+
+
